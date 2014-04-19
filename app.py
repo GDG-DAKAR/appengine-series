914faa7819ca54de34ae4399a016015089298f17
Hassane Moustapha <hassanemoustapha@gmail.com>
2014-04-19 17:11:56 +0000

    fin du meetup 1

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 # *coding: utf-8*
 import webapp2
-import jinja2
 import json
+import jinja2
 import os
+
+from google.appengine.ext import db
+
+class Personne(db.Model):
+  nom = db.StringProperty()
 
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class Accueil(webapp2.RequestHandler):
   def get(self):
-    template_values = {}
+    lenom = self.request.get('nom')
+    p = Personne(nom = lenom)
+    p.put()
     template = jinja_environment.get_template('views/index.html')
-    self.response.out.write(template.render(template_values))
+    self.response.write(template.render( {} ))
+
+  def post(self):
+    le_nom = self.request.get('nom', '')
+    self.response.write( le_nom )
 
 class JSONApi(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'application/json'
-    objet = { 'yes' : 'tu vas comprendre' }
-    self.response.write(json.dumps(objet))
+    token = self.request.get('token', None)
+    if token:
+      res = [ { 'nom' : p.nom } for p in Personne.all() ]
+    else:
+      res = { 'code': 1, 'message': "Tu fais quoi ici ? hein" }
 
-routes = [('/', Accueil), ('/api', JSONApi)]
+    self.response.headers["Content-Type"] = "application/json"
+    self.response.write( json.dumps( res ) )
 
+routes = [("/", Accueil), ("/api", JSONApi)]
 app = webapp2.WSGIApplication( routes, debug="-->" )
